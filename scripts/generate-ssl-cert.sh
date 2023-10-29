cb9f1f7563e7f8e22af0580693f3bdc4640b905d
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-10-29 23:29:31 +1000

    Update generate-ssl-cert.sh
    link environment profile variables to script

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+source /etc/profile
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 DEFAULT_LOG_FILE=$DIR/var/log/zerossl/zerossl.log-$(date '+%s')
 KEYS_DIR="$DIR/var/lib/jelastic/keys/"

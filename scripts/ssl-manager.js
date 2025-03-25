f3895684d0d629366f21169551fd291e92afed19
Ayush <ayush@rapyd.cloud>
2025-03-25 12:17:10 +0530

    test

--- scripts/ssl-manager.js
+++ scripts/ssl-manager.js
@@ -479,7 +479,7 @@ function SSLManager(config) {
             return me.exec(me.sendEmail, "Action Required", "html/update-required.html");
         }
 
-        return {"result":0,"amazingtest":111,"session":session}
+        return {"result":0,"amazingtest":111,"session123":session}
 
         if (!config.isTask) {
             me.logAction("StartUpdateLEFromContainer");

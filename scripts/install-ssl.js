56507dcc4330d5d9a09ce48896142c1199f5ffa1
Ayush <ayush@rapyd.cloud>
2025-03-25 11:46:41 +0530

    test

--- scripts/install-ssl.js
+++ scripts/install-ssl.js
@@ -1,5 +1,80 @@
 //@url('/${scriptName}')
 
-jelastic.marketplace.console.WriteLog("Session from ZERO SSL plugin - " + session);
 
-return { "result": 0 , "session": session };
+var action = getParam("action", "install"),
+    patchVersion  = getParam("patchVersion"),
+    token  = getParam("token"),
+    isTask = getParam("task"),
+    baseUrl = "${baseUrl}";
+
+function run() {
+    var SSLManager = use("scripts/ssl-manager.js", {
+        session          : session,
+        isTask           : isTask,
+        baseUrl          : baseUrl,
+        action           : action,
+        
+        token            : "${token}",
+        email            : "${email}",
+        baseDir          : "${baseDir}",
+        scriptName       : "${scriptName}",
+        envName          : "${envName}",
+        envDomain        : "${envDomain}",
+
+        appId            : "${appId}",
+        envAppid         : "${envAppid}",
+        nodeId           : "${nodeId}",
+        nodeIp           : "${nodeIp}",
+        nodeGroup        : "${nodeGroup}",
+        customDomains    : "${customDomains}",
+        cronTime         : "${cronTime}",
+        deployHook       : "${deployHook}",
+        deployHookType   : "${deployHookType}",
+        undeployHook     : "${undeployHook}",
+        undeployHookType : "${undeployHookType}",
+        withExtIp        : "${withExtIp}",
+        webroot          : "${webroot}",
+        webrootPath      : "${webrootPath}",
+        fallbackToX1     : "${fallbackToX1}",
+        patchVersion     : "${patchVersion}",
+        test             : "${test}",
+        clientVersion    : "${clientVersion}"
+    });
+
+    var resp = SSLManager.auth(token);
+
+    if (resp.result === 0) {
+        resp = SSLManager.invoke(action);
+    }
+
+    jelastic.local.ReturnResult(resp);
+}
+
+function use(script, config) {
+    var Transport = com.hivext.api.core.utils.Transport,
+        body = new Transport().get(baseUrl + "/" + script + "?_r=" + Math.random());
+    
+    if (!body) {
+        return {
+            result : com.hivext.api.Response.ERROR_UNKNOWN,
+            error: "Error: Unable to get ssl-manager script from GitHub"
+        }
+    }
+
+    return new (new Function("return " + body)())(config);
+}
+
+try {
+    run();
+} catch (ex) {
+    var resp = {
+        result : com.hivext.api.Response.ERROR_UNKNOWN,
+        error: "Error: " + toJSON(ex)
+    };
+
+    if (jelastic.marketplace && jelastic.marketplace.console) {
+        jelastic.marketplace.console.WriteLog(appid, signature, "ERROR: " + resp);
+    }
+
+    jelastic.local.ReturnResult(resp);
+}
